3c8fdcb35645ae52d6459e736ef3a48af4a307e4
HevertonMac <hevertonmacedo23@gmail.com>
2023-08-16 19:23:34 -0300

    correcao identacao

--- Q1_Vector.java
+++ Q1_Vector.java
@@ -2,9 +2,9 @@
 public class Q1_Vector {
 	public static void main(String[] args) {
 		Bigmax aux = new Bigmax();
-        String[] palavras = aux.getPalavavrasDoTXT("leipzig100k.txt");
-        System.out.println("Numero de palavras splitadas: " + palavras.length);
-        palavras = aux.eliminaPalavrasRepitidas(palavras);
-        System.out.println("Numero de palavras únicas splitadas: " + palavras.length);
+		String[] palavras = aux.getPalavavrasDoTXT("leipzig100k.txt");
+		System.out.println("Numero de palavras splitadas: " + palavras.length);
+		palavras = aux.eliminaPalavrasRepitidas(palavras);
+		System.out.println("Numero de palavras únicas splitadas: " + palavras.length);
 	}
 }
