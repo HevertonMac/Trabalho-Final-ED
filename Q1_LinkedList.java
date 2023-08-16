55b6ea9c95c21bf9275249fe1b6a81ae4046e749
HevertonMac <hevertonmacedo23@gmail.com>
2023-08-16 19:17:03 -0300

    teste2

--- Q1_LinkedList.java
+++ Q1_LinkedList.java
@@ -1,6 +1,7 @@
 
 public class Q1_LinkedList {
 	public static void main(String[] args) {
+		//mesmo modelo de execução para as outras classes
 		Bigmax aux = new Bigmax();
         String[] palavras = aux.getPalavavrasDoTXT("leipzig100k.txt");
         System.out.println("Numero de palavras splitadas: " + palavras.length);
