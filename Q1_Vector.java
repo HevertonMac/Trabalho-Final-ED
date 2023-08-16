7225b1d01c0e9181c140601b6cc08eb9b93c46d3
HevertonMac <hevertonmacedo23@gmail.com>
2023-08-16 19:17:59 -0300

    its all folks

--- Q1_Vector.java
+++ Q1_Vector.java
@@ -1,7 +1,6 @@
 
 public class Q1_Vector {
 	public static void main(String[] args) {
-		//mesmo modelo de execução para as outras classes
 		Bigmax aux = new Bigmax();
         String[] palavras = aux.getPalavavrasDoTXT("leipzig100k.txt");
         System.out.println("Numero de palavras splitadas: " + palavras.length);
